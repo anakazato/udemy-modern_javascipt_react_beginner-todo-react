4174bd03c72450a6a395c9005e76f05fa13d2b0c
anakazato <aki.nakazato.office@gmail.com>
2025-12-12 03:15:55 +0900

    追加処理

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import './styles.css';
 
 export const Todo = () => {
+    const [todoText, setTodoText] = useState("");
     const [incompleteTodos, setIncompleteTodos] = useState([
         "TODOです１",
         "TODOです２"
@@ -11,12 +12,26 @@ export const Todo = () => {
         "TODOでした１",
         "TODOでした２"
     ]);
-    
+
+    const onChageTodoText = (event) => setTodoText(event.target.value);
+    const onClickAdd = () => {
+        if(todoText === "") return;
+        if(incompleteTodos.findIndex(todo => todo == todoText) != -1){
+            alert("同じのはダメだってば！");
+            return;
+        }
+        const newTodos = [...incompleteTodos,todoText];
+        setIncompleteTodos(newTodos);
+        setTodoText("");
+    }
     return (
         <>
             <div className="input-area">
-                <input placeholder='TODOを入力' />
-                <button>追加</button>
+                <input
+                    placeholder='TODOを入力'
+                    value={todoText} 
+                    onChange={onChageTodoText} />
+                <button onClick={onClickAdd}>追加</button>
             </div>
             <div className='incomplete-area'>
                 <p className='title'>未完了のTODO</p>
